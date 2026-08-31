c0ecaa2129f270c4b3fc76c4d5b6ad6560d459e6
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2026-08-31 20:26:17 -0300

    QUEST-08 - [feature]: adiciona o cadastro de produtos e livros

---
+++ src/questao08/entities/Livro.java
@@ -0,0 +1,16 @@
+package questao08.entities;
+
+public class Livro extends Produto {
+
+    private String autor;
+
+    public Livro(String nome, double preco, String autor){
+        super(nome, preco);
+        this.autor = autor;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+}
---
+++ src/questao08/entities/Produto.java
@@ -0,0 +1,28 @@
+package questao08.entities;
+
+public class Produto {
+
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    private void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
